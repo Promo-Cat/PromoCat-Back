cbb2813b04de9b8daad63e3617a2cf28f8ac9498
Maxim <maximgran@gmail.com>
2020-05-05 12:28:18 +0300

    First commit

---
+++ src/main/java/org/promocat/promocat/PromoCatApplication.java
@@ -0,0 +1,13 @@
+package org.promocat.promocat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoCatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromoCatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/promocat/promocat/PromocatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.promocat.promocat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromocatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
