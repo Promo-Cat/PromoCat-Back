ababb756a51baa33cffe9b7e564617ad4e8c8d2c
Maxim <maximgran@gmail.com>
2020-05-07 14:25:53 +0300

    exception structure fixed

--- src/main/java/org/promocat/promocat/exception/ApiExceptionHandler.java
+++ src/main/java/org/promocat/promocat/exception/ApiExceptionHandler.java
@@ -1,19 +1,12 @@
 package org.promocat.promocat.exception;
 
-import org.promocat.promocat.exception.validation.ApiFieldException;
-import org.promocat.promocat.exception.validation.ApiValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Grankin Maxim (maximgran@gmail.com) at 11:49 07.05.2020
@@ -30,21 +23,5 @@ public class ApiExceptionHandler {
                 ZonedDateTime.now(ZoneId.of("Z"))
         );
         return new ResponseEntity<>(apiException, badRequest);
-    }
-
-    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
-    public ResponseEntity<Object> validationError(MethodArgumentNotValidException ex) {
-        BindingResult result = ex.getBindingResult();
-        final List<FieldError> fieldErrors = result.getFieldErrors();
-        List<ApiFieldException> validationExceptionList = new ArrayList<>();
-        for (FieldError fieldError : fieldErrors) {
-            validationExceptionList.add(new ApiFieldException(
-                    fieldError.getField(),
-                    fieldError.getRejectedValue(),
-                    fieldError.getDefaultMessage()
-            ));
-        }
-
-        return new ResponseEntity<>(new ApiValidationException(validationExceptionList), HttpStatus.BAD_REQUEST);
     }
 }
