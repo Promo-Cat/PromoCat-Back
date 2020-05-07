2388a974b9bc3226fcccf24a4a784eb69bf1ed3f
Maxim <maximgran@gmail.com>
2020-05-07 14:54:53 +0300

    media type exception added

--- src/main/java/org/promocat/promocat/data_entities/user/UserController.java
+++ src/main/java/org/promocat/promocat/data_entities/user/UserController.java
@@ -7,9 +7,12 @@ import org.promocat.promocat.data_entities.car.CarRecord;
 import org.promocat.promocat.data_entities.car.dto.CarDTO;
 import org.promocat.promocat.data_entities.promo_code.PromoCodeController;
 import org.promocat.promocat.data_entities.user.dto.UserDTO;
+import org.promocat.promocat.exception.ApiException;
 import org.promocat.promocat.exception.validation.ApiValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,11 +34,31 @@ public class UserController {
         this.userService = userService;
     }
 
+    public static UserRecord userDTOToRecord(final UserDTO userDTO) {
+        UserRecord userRecord = new UserRecord();
+        userRecord.setId(userDTO.getId());
+        userRecord.setFirst_name(userDTO.getFirstName());
+        userRecord.setLast_name(userDTO.getLastName());
+        userRecord.setTelephone(userDTO.getTelephone());
+        userRecord.setToken(userDTO.getToken());
+        userRecord.setBalance(userDTO.getBalance());
+        List<CarRecord> cars = new ArrayList<>();
+        for (CarDTO carDTO : userDTO.getCars()) {
+            cars.add(CarController.carDTOToRecord(carDTO, userRecord));
+        }
+        userRecord.setCars(cars);
+        userRecord.setPromo_code(PromoCodeController.promoCodeDTOToRecord(userDTO.getPromoCodeDTO(), userRecord));
+        return userRecord;
+    }
+
     @ApiResponses(value = {
             @ApiResponse(code = 400,
                     message = "Bad Request",
-                    response = ApiValidationException.class)})
-    @PostMapping(path = "/addUser", consumes = "application/json")
+                    response = ApiValidationException.class),
+            @ApiResponse(code = 415,
+                    message = "Not acceptable media type",
+                    response = ApiException.class)})
+    @PostMapping(path = "/addUser", consumes = MediaType.APPLICATION_JSON_VALUE)
     public UserDTO addUser(@Valid @RequestBody UserRecord user) {
         return userService.save(user);
     }
@@ -52,22 +75,5 @@ public class UserController {
         } catch (UsernameNotFoundException e) {
             return null; // TODO: Ошибка
         }
-    }
-
-    public static UserRecord userDTOToRecord(final UserDTO userDTO) {
-        UserRecord userRecord = new UserRecord();
-        userRecord.setId(userDTO.getId());
-        userRecord.setFirst_name(userDTO.getFirstName());
-        userRecord.setLast_name(userDTO.getLastName());
-        userRecord.setTelephone(userDTO.getTelephone());
-        userRecord.setToken(userDTO.getToken());
-        userRecord.setBalance(userDTO.getBalance());
-        List<CarRecord> cars = new ArrayList<>();
-        for (CarDTO carDTO : userDTO.getCars()) {
-            cars.add(CarController.carDTOToRecord(carDTO, userRecord));
-        }
-        userRecord.setCars(cars);
-        userRecord.setPromo_code(PromoCodeController.promoCodeDTOToRecord(userDTO.getPromoCodeDTO(), userRecord));
-        return userRecord;
     }
 }
