cb0ff932c0b07acadd64c7f8fa9a578b7c4f39d3
Maxim <maximgran@gmail.com>
2020-05-09 23:32:25 +0300

    not null message for telephone in userrecord added

--- src/main/java/org/promocat/promocat/data_entities/user/UserRecord.java
+++ src/main/java/org/promocat/promocat/data_entities/user/UserRecord.java
@@ -59,7 +59,7 @@ public class UserRecord {
     /**
      * Телефон пользователя.
      */
-    @NotBlank
+    @NotBlank(message = "Телефон не может быть пустым")
     @Pattern(regexp = "\\+7\\(\\d{3}\\)\\d{3}-\\d{2}-\\d{2}",
             message = "Телефон должен соответствовать шаблону +X(XXX)XXX-XX-XX")
     @Column(unique = true)
