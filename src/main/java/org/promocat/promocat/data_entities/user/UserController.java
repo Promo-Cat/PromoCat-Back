4742b2b3fff3d4da8fd115db7071bf48a308d26c
Roman Devyatilov <r.devyatilov@yandex.ru>
2020-05-10 11:53:29 +0300

    added production properties with psql

--- src/main/java/org/promocat/promocat/data_entities/user/UserController.java
+++ src/main/java/org/promocat/promocat/data_entities/user/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
