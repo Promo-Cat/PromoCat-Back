1b19f03dbb59dd786295dbb4173ceb0279f96366
DanLys <lyskin-2013@mail.ru>
2020-05-12 18:44:57 +0300

    StockRecord

--- src/main/java/org/promocat/promocat/data_entities/stock/StockRecord.java
+++ src/main/java/org/promocat/promocat/data_entities/stock/StockRecord.java
@@ -3,11 +3,9 @@ package org.promocat.promocat.data_entities.stock;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
 
 @Data
 @Entity
