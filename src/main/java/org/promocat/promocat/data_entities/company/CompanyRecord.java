06cf823bf35ff7733afdb85502bba564fe0f21b9
DanLys <lyskin-2013@mail.ru>
2020-05-12 17:57:43 +0300

    Company entity

--- src/main/java/org/promocat/promocat/data_entities/company/CompanyRecord.java
+++ src/main/java/org/promocat/promocat/data_entities/company/CompanyRecord.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Data
 @NoArgsConstructor(force = true)
@@ -12,13 +13,75 @@ import javax.validation.constraints.NotBlank;
 @Table(name = "company")
 public class CompanyRecord {
 
+    /**
+     * id компании.
+     */
     @Id
     @Column(unique = true)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long company_id;
 
-    @NotBlank
+    /**
+     * Имя компании.
+     */
+    @NotBlank(message = "Имя организации не может быть пустым.")
     @Column
-    private String company_name;
+    private String organization_name;
 
+    /**
+     * Имя руководителя.
+     */
+    @NotBlank(message = "Имя руководителя не может быть пустым.")
+    @Column
+    private String user_first_name;
+
+    /**
+     * Фамилия руководителя.
+     */
+    @NotBlank(message = "Фамилия руководителя не может быть пустой.")
+    @Column
+    private String user_second_name;
+
+    /**
+     * Отчество руководителя.
+     */
+    @Column
+    private String user_patronymic;
+
+    /**
+     * ОГРН компании.
+     */
+    @NotBlank(message = "ОГРН организации не может быть пустым.")
+    @Column
+    private String ogrn;
+
+    /**
+     * ИНН компании.
+     */
+    @NotBlank(message = "ИНН организации не может быть пустым.")
+    @Column
+    private String inn;
+
+    /**
+     * Телефон руководителя.
+     */
+    @NotBlank(message = "Телефон не может быть пустым.")
+    @Pattern(regexp = "\\+7\\(\\d{3}\\)\\d{3}-\\d{2}-\\d{2}",
+            message = "Телефон должен соответствовать шаблону +X(XXX)XXX-XX-XX")
+    @Column
+    private String telephone;
+
+    /**
+     * Почта руководителя.
+     */
+    @NotBlank(message = "Имя почты не может быть пустым.")
+    @Column
+    private String mail;
+
+    /**
+     * Город.
+     */
+    @NotBlank(message = "Город не может быть пустым.")
+    @Column
+    private String city;
 }
