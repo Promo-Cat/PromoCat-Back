a7ff5b55a6c716478d41ea4e4ee1857f09e6adcb
Maxim <maximgran@gmail.com>
2020-05-15 21:33:44 +0300

    mapper fixed

--- src/main/java/org/promocat/promocat/mapper/AbstractAccountMapper.java
+++ src/main/java/org/promocat/promocat/mapper/AbstractAccountMapper.java
@@ -3,10 +3,13 @@ package org.promocat.promocat.mapper;
 import org.promocat.promocat.data_entities.AbstractAccount;
 import org.promocat.promocat.dto.AbstractAccountDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-public class AbstractAccountMapper extends AbstractMapper<AbstractAccount, AbstractAccountDTO>{
+@Component
+public class AbstractAccountMapper extends AbstractMapper<AbstractAccount, AbstractAccountDTO> {
+
     @Autowired
-    public AbstractAccountMapper(Class<AbstractAccount> entityClass, Class<AbstractAccountDTO> dtoClass) {
-        super(entityClass, dtoClass);
+    public AbstractAccountMapper() {
+        super(AbstractAccount.class, AbstractAccountDTO.class);
     }
 }
