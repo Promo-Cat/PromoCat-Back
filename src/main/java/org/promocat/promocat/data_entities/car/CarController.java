1e85b968bcd7d0401f3f2ff74faeb2ad19068e1d
Maxim <maximgran@gmail.com>
2020-05-17 16:37:50 +0300

    car&stock swagger apiresponses added

--- src/main/java/org/promocat/promocat/data_entities/car/CarController.java
+++ src/main/java/org/promocat/promocat/data_entities/car/CarController.java
@@ -1,7 +1,12 @@
 package org.promocat.promocat.data_entities.car;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.promocat.promocat.dto.CarDTO;
+import org.promocat.promocat.exception.ApiException;
+import org.promocat.promocat.exception.validation.ApiValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,6 +30,21 @@ public class CarController {
         this.carService = carService;
     }
 
+    @ApiOperation(value = "Add car",
+            notes = "Adds stock for company with id specified in request.",
+            response = CarDTO.class,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses(value = {
+            @ApiResponse(code = 400,
+                    message = "Validation error",
+                    response = ApiValidationException.class),
+            @ApiResponse(code = 415,
+                    message = "Not acceptable media type",
+                    response = ApiException.class),
+            @ApiResponse(code = 406,
+                    message = "Some DB problems",
+                    response = ApiException.class)
+    })
     @RequestMapping(path = "/api/user/car", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public CarDTO addCar(@Valid @RequestBody CarDTO car) {
         log.info(String.format("Trying to add car to user: %d", car.getUserId()));
--- src/main/java/org/promocat/promocat/data_entities/stock/StockController.java
+++ src/main/java/org/promocat/promocat/data_entities/stock/StockController.java
@@ -1,7 +1,13 @@
 package org.promocat.promocat.data_entities.stock;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.promocat.promocat.dto.StockDTO;
+import org.promocat.promocat.dto.UserDTO;
+import org.promocat.promocat.exception.ApiException;
+import org.promocat.promocat.exception.validation.ApiValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,13 +31,43 @@ public class StockController {
         this.stockService = stockService;
     }
 
-    @RequestMapping(path = "/auth/company/stock", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public StockDTO addCar(@Valid @RequestBody StockDTO stock) {
+    @ApiOperation(value = "Create stock",
+            notes = "Adds stock for company with id specified in request.",
+            response = StockDTO.class,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses(value = {
+            @ApiResponse(code = 400,
+                    message = "Validation error",
+                    response = ApiValidationException.class),
+            @ApiResponse(code = 415,
+                    message = "Not acceptable media type",
+                    response = ApiException.class),
+            @ApiResponse(code = 406,
+                    message = "Some DB problems",
+                    response = ApiException.class)
+    })
+    @RequestMapping(path = "/api/stock", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public StockDTO addStock(@Valid @RequestBody StockDTO stock) {
         log.info(String.format("Trying to save stock from company: %d", stock.getCompanyId()));
         return stockService.save(stock);
     }
 
-    @RequestMapping(path = "/auth/stock/getById", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE)
+    // TODO admin
+    @ApiOperation(value = "Get stock",
+            notes = "Returning stock",
+            response = UserDTO.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 404,
+                    message = "Stock not found",
+                    response = ApiException.class),
+            @ApiResponse(code = 415,
+                    message = "Not acceptable media type",
+                    response = ApiException.class),
+            @ApiResponse(code = 406,
+                    message = "Some DB problems",
+                    response = ApiException.class)
+    })
+    @RequestMapping(path = "/api/stock/getById", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE)
     public StockDTO getStockById(@RequestBody Long id) {
         return stockService.findById(id);
     }
