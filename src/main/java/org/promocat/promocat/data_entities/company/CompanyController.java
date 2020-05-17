7768da21a8065dde3a55a3728fc6003957b21dcc
Maxim <maximgran@gmail.com>
2020-05-17 16:39:49 +0300

    company swagger added

--- src/main/java/org/promocat/promocat/data_entities/company/CompanyController.java
+++ src/main/java/org/promocat/promocat/data_entities/company/CompanyController.java
@@ -1,7 +1,12 @@
 package org.promocat.promocat.data_entities.company;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.promocat.promocat.dto.CompanyDTO;
+import org.promocat.promocat.exception.ApiException;
+import org.promocat.promocat.exception.validation.ApiValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,6 +30,21 @@ public class CompanyController {
         this.companyService = companyService;
     }
 
+    @ApiOperation(value = "Register company",
+            notes = "Registering company with telephone in format +X(XXX)XXX-XX-XX",
+            response = CompanyDTO.class,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses(value = {
+            @ApiResponse(code = 400,
+                    message = "Validation error",
+                    response = ApiValidationException.class),
+            @ApiResponse(code = 415,
+                    message = "Not acceptable media type",
+                    response = ApiException.class),
+            @ApiResponse(code = 406,
+                    message = "Some DB problems",
+                    response = ApiException.class)
+    })
     @RequestMapping(path = "/auth/register/company", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public CompanyDTO addCompany(@Valid @RequestBody CompanyDTO company) {
         log.info(String.format("Trying to save company: %s. Organization telephone: %s",
