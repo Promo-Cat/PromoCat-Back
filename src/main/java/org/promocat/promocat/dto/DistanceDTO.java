f9122cea78bf1c034b36dec2d283ae1724b44e4f
Maxim <maximgran@gmail.com>
2020-05-30 20:38:19 +0300

    distance and movement documentated

--- src/main/java/org/promocat/promocat/dto/DistanceDTO.java
+++ src/main/java/org/promocat/promocat/dto/DistanceDTO.java
@@ -1,17 +1,37 @@
 package org.promocat.promocat.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
+@ApiModel(
+        value = "Distance",
+        description = "Record of the distance traveled by the user"
+)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class DistanceDTO {
 
-    private Double distance;
+    @ApiModelProperty(
+            value = "Date",
+            dataType = "LocalDate in format {YYYY-MM-DD}",
+            required = true
+    )
+    @NotNull(message = "Дата не может быть пустой.")
     private LocalDate date;
+
+    @ApiModelProperty(
+            value = "Distance",
+            dataType = "Double",
+            required = true
+    )
+    @NotNull(message = "Дистанция не может быть пустой.")
+    private Double distance;
 
 }
--- src/main/java/org/promocat/promocat/dto/MovementDTO.java
+++ src/main/java/org/promocat/promocat/dto/MovementDTO.java
@@ -1,5 +1,7 @@
 package org.promocat.promocat.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -12,18 +14,45 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
 
+@ApiModel(
+        value = "Movement",
+        description = "Object representation users movement."
+)
 @EqualsAndHashCode(of = {}, callSuper = true)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class MovementDTO extends AbstractDTO {
 
+    @ApiModelProperty(
+            value = "Users id",
+            dataType = "Long",
+            required = true
+    )
+    @NotNull(message = "Id пользователя не может быть пустым.")
     private Long userId;
+
+    @ApiModelProperty(
+            value = "Stock id",
+            dataType = "Long",
+            required = true
+    )
+    @NotNull(message = "Id акции не может быть пустым.")
     private Long stockId;
 
+    @ApiModelProperty(
+            value = "Date",
+            dataType = "LocalDate in format {YYYY-MM-DD}",
+            required = true
+    )
     @NotNull(message = "Дата не может быть пустой.")
     private LocalDate date;
 
+    @ApiModelProperty(
+            value = "Distance",
+            dataType = "Double",
+            required = true
+    )
     @NotNull(message = "Дистанция не может быть пустой.")
     private Double distance;
 
