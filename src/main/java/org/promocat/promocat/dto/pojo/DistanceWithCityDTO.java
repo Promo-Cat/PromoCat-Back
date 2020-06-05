9df2886bfea89a34c6c9e576aa80b8e9ce392767
Maxim <maximgran@gmail.com>
2020-06-05 20:10:33 +0300

    some docs added

--- src/main/java/org/promocat/promocat/dto/pojo/DistanceWithCityDTO.java
+++ src/main/java/org/promocat/promocat/dto/pojo/DistanceWithCityDTO.java
@@ -1,5 +1,7 @@
 package org.promocat.promocat.dto.pojo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -7,19 +9,29 @@ import lombok.NoArgsConstructor;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
+@ApiModel(
+        value = "Distance with city",
+        description = "Distance in city."
+)
 @Data
 @EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 public class DistanceWithCityDTO extends DistanceDTO {
 
+    @ApiModelProperty(
+            value = "City id",
+            dataType = "Long",
+            required = true
+    )
     private Long cityId;
 
-    public DistanceWithCityDTO(LocalDate date, Double distance, Long cityId) {
+    public DistanceWithCityDTO(final LocalDate date, final Double distance, final Long cityId) {
         super(date, distance);
         this.cityId = cityId;
     }
 
-    public DistanceWithCityDTO(@NotNull(message = "Дистанция не может быть пустой.") Double distance, Long cityId) {
+    public DistanceWithCityDTO(final @NotNull(message = "Дистанция не может быть пустой.") Double distance,
+                               final Long cityId) {
         super(distance);
         this.cityId = cityId;
     }
