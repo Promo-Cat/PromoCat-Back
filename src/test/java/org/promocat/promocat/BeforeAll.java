6860bd3b8b43c6e60ea948cd7f4268370757814d
DanLys <lyskin-2013@mail.ru>
2020-07-13 13:41:09 +0300

    new car tests create with City and User in BeforeAll

--- src/test/java/org/promocat/promocat/BeforeAll.java
+++ src/test/java/org/promocat/promocat/BeforeAll.java
@@ -1,18 +1,32 @@
 package org.promocat.promocat;
 
+import org.promocat.promocat.attributes.AccountType;
+import org.promocat.promocat.data_entities.AbstractAccount;
 import org.promocat.promocat.data_entities.admin.AdminRepository;
 import org.promocat.promocat.data_entities.car.CarRepository;
+import org.promocat.promocat.data_entities.city.City;
 import org.promocat.promocat.data_entities.city.CityRepository;
 import org.promocat.promocat.data_entities.company.CompanyRepository;
+import org.promocat.promocat.data_entities.login_attempt.LoginAttemptService;
 import org.promocat.promocat.data_entities.movement.MovementRepository;
 import org.promocat.promocat.data_entities.parameters.ParametersRepository;
 import org.promocat.promocat.data_entities.promo_code.PromoCodeRepository;
 import org.promocat.promocat.data_entities.promocode_activation.PromoCodeActivationRepository;
 import org.promocat.promocat.data_entities.stock.StockRepository;
+import org.promocat.promocat.data_entities.user.User;
 import org.promocat.promocat.data_entities.user.UserRepository;
+import org.promocat.promocat.dto.LoginAttemptDTO;
+import org.promocat.promocat.dto.UserDTO;
+import org.promocat.promocat.dto.pojo.AuthorizationKeyDTO;
+import org.promocat.promocat.dto.pojo.TokenDTO;
+import org.promocat.promocat.mapper.UserMapper;
+import org.promocat.promocat.util_entities.TokenService;
+import org.promocat.promocat.utils.AccountRepositoryManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 /**
  * Created by Danil Lyskin at 10:36 13.07.2020
@@ -52,8 +66,23 @@ public class BeforeAll {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    AccountRepositoryManager accountRepositoryManager;
+
+    @Autowired
+    LoginAttemptService loginAttemptService;
+
+    @Autowired
+    TokenService tokenService;
+
+    @Autowired
+    UserMapper userMapper;
+
+    public String userToken;
+    public UserDTO userDTO;
+
     /**
-     * Отчистка БД.
+     * Отчистка и заполнение БД.
      */
     public void init() {
         adminRepository.deleteAll();
@@ -65,5 +94,35 @@ public class BeforeAll {
         promoCodeActivationRepository.deleteAll();
         stockRepository.deleteAll();
         userRepository.deleteAll();
+        create();
+    }
+
+    /**
+     * Заполнение БД.
+     */
+    private void create() {
+        // Активация города в БД.
+        Optional<City> op = cityRepository.findById(1L);
+        City city = op.get();
+        city.setActive(true);
+        cityRepository.save(city);
+
+        // Добавление пользователя в БД + токен.
+        User user = new User();
+        user.setTelephone("+7(111)111-11-11");
+        user.setMail("rtyui@mail.ru");
+        user.setCity(city);
+        user.setAccountType(AccountType.USER);
+        user = userRepository.save(user);
+        userDTO = userMapper.toDto(user);
+
+        Optional<? extends AbstractAccount> account = accountRepositoryManager.getRepository(AccountType.USER).getByTelephone("+7(111)111-11-11");
+        AuthorizationKeyDTO key = loginAttemptService.login(account.get());
+
+        LoginAttemptDTO loginAttempt = new LoginAttemptDTO(key.getAuthorizationKey(), "1337");
+        Optional<? extends AbstractAccount> accountRecord = loginAttemptService.checkLoginAttemptCode(loginAttempt);
+        AbstractAccount accountToken = accountRecord.get();
+        TokenDTO userTokenDTO = new TokenDTO(tokenService.getToken(accountToken.getTelephone(), accountToken.getAccountType()));
+        userToken = userTokenDTO.getToken();
     }
 }
--- src/test/java/org/promocat/promocat/car/CarTest.java
+++ src/test/java/org/promocat/promocat/car/CarTest.java
@@ -1,121 +1,102 @@
-//package org.promocat.promocat.car;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.promocat.promocat.dto.CarDTO;
-//import org.promocat.promocat.dto.UserDTO;
-//import org.promocat.promocat.dto.pojo.AuthorizationKeyDTO;
-//import org.promocat.promocat.dto.pojo.TokenDTO;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.context.ActiveProfiles;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.test.context.web.WebAppConfiguration;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.MvcResult;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-///**
-// * Created by Danil Lyskin at 14:43 20.05.2020
-// */
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@WebAppConfiguration
-//@SpringBootTest
-//@ActiveProfiles("test")
-//@AutoConfigureMockMvc
-//public class CarTest {
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    private UserDTO user;
-//    private String token;
-//    private String adminToken;
-//
-//    @Before
-//    public void init() throws Exception {
-//        user = new UserDTO();
-//        user.setMail("I");
-//        user.setTelephone("+7(999)243-26-99");
-//        user.setCityId(2L);
-//        user.setId(1L);
-//        this.mockMvc.perform(post("/auth/user/register").contentType(MediaType.APPLICATION_JSON)
-//                .content(new ObjectMapper().writeValueAsString(user)))
-//                .andExpect(status().isOk());
-//        MvcResult key = this.mockMvc.perform(get("/auth/user/login?telephone=+7(999)243-26-99"))
-//                .andExpect(status().isOk())
-//                .andReturn();
-//        MvcResult tokenR = this.mockMvc.perform(get("/auth/token?authorizationKey="
-//                + new ObjectMapper().readValue(key.getResponse().getContentAsString(), AuthorizationKeyDTO.class).getAuthorizationKey()
-//                + "&code=1337")).andExpect(status().isOk())
-//                .andReturn();
-//        token = new ObjectMapper().readValue(tokenR.getResponse().getContentAsString(), TokenDTO.class).getToken();
-//
-//        key = this.mockMvc.perform(get("/auth/admin/login?telephone=+7(999)243-26-99"))
-//                .andExpect(status().isOk()).andReturn();
-//        tokenR = this.mockMvc.perform(get("/auth/token?authorizationKey="
-//                + new ObjectMapper().readValue(key.getResponse().getContentAsString(), AuthorizationKeyDTO.class).getAuthorizationKey()
-//                + "&code=1337")).andExpect(status().isOk())
-//                .andReturn();
-//        adminToken = new ObjectMapper().readValue(tokenR.getResponse().getContentAsString(), TokenDTO.class).getToken();
-//        this.mockMvc.perform(put("/data/examples/admin/city/active?city=Змеиногорск").header("token", adminToken))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    public void testSaveCarWithoutNumber() throws Exception {
-//        CarDTO car = new CarDTO();
-//        car.setRegion("26");
-//        car.setUserId(1L);
-//
-//        this.mockMvc.perform(post("/api/user/car").header("token", token).contentType(MediaType.APPLICATION_JSON_VALUE)
-//                .content(new ObjectMapper().writeValueAsString(car)))
-//                .andExpect(status().is4xxClientError());
-//    }
-//
-//    @Test
-//    public void testSaveCarWithoutRegion() throws Exception {
-//        CarDTO car = new CarDTO();
-//        car.setNumber("awfawfaf");
-//        car.setUserId(1L);
-//
-//        this.mockMvc.perform(post("/api/user/car").header("token", token).contentType(MediaType.APPLICATION_JSON_VALUE)
-//                .content(new ObjectMapper().writeValueAsString(car)))
-//                .andExpect(status().is4xxClientError());
-//    }
-//
-//    @Test
-//    public void testSaveCarWithoutUserId() throws Exception {
-//        CarDTO car = new CarDTO();
-//        car.setNumber("awfawfaf");
-//        car.setRegion("26");
-//
-//        this.mockMvc.perform(post("/api/user/car").header("token", token).contentType(MediaType.APPLICATION_JSON_VALUE)
-//                .content(new ObjectMapper().writeValueAsString(car)))
-//                .andExpect(status().is4xxClientError());
-//    }
-//
-//    @Test
-//    public void testSaveCarWithAllCorrect() throws Exception {
-//        CarDTO car = new CarDTO();
-//        car.setNumber("awfawfaf");
-//        car.setRegion("26");
-//        car.setUserId(user.getId());
-//
-//        this.mockMvc.perform(post("/api/user/car").header("token", token).contentType(MediaType.APPLICATION_JSON_VALUE)
-//                .content(new ObjectMapper().writeValueAsString(car)))
-//                .andExpect(status().isOk());
-//    }
-//
+package org.promocat.promocat.car;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.promocat.promocat.BeforeAll;
+import org.promocat.promocat.data_entities.user.UserService;
+import org.promocat.promocat.dto.CarDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by Danil Lyskin at 14:43 20.05.2020
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+public class CarTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    BeforeAll beforeAll;
+
+    @Autowired
+    UserService userService;
+
+    @Before
+    public void clear() {
+        beforeAll.init();
+    }
+
+    /**
+     * Добавление машины без Номера.
+     *
+     * @throws Exception Машина не сохраняется, ошибка 400.
+     */
+    @Test
+    public void testSaveCarWithoutNumber() throws Exception {
+        CarDTO car = new CarDTO();
+        car.setRegion("26");
+
+        this.mockMvc.perform(post("/api/user/car").header("token", beforeAll.userToken).contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(new ObjectMapper().writeValueAsString(car)))
+                .andExpect(status().is4xxClientError());
+    }
+
+    /**
+     * Добавление машины без региона.
+     *
+     * @throws Exception Машина не сохранятеся, ошибка 400.
+     */
+    @Test
+    public void testSaveCarWithoutRegion() throws Exception {
+        CarDTO car = new CarDTO();
+        car.setNumber("awfawfaf");
+
+        this.mockMvc.perform(post("/api/user/car").header("token", beforeAll.userToken).contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(new ObjectMapper().writeValueAsString(car)))
+                .andExpect(status().is4xxClientError());
+    }
+
+    /**
+     * Добавление машины.
+     */
+    @Test
+    public void testSaveCarWithAllCorrect() throws Exception {
+        CarDTO car = new CarDTO();
+        car.setNumber("awfawfaf");
+        car.setRegion("26");
+
+        MvcResult result = this.mockMvc.perform(post("/api/user/car").header("token", beforeAll.userToken).contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(new ObjectMapper().writeValueAsString(car)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        CarDTO that = new ObjectMapper().readValue(result.getResponse().getContentAsString(), CarDTO.class);
+        assertNotNull(that.getId());
+        assertEquals(car.getNumber(), that.getNumber());
+        assertEquals(car.getRegion(), that.getRegion());
+        assertEquals(that.getUserId(), beforeAll.userDTO.getId());
+    }
+
 //    private CarDTO save(String number) throws Exception {
 //        CarDTO car = new CarDTO();
 //        car.setNumber(number);
@@ -175,4 +156,4 @@
 //        assertEquals(car.getRegion(), carRes.getRegion());
 //        assertEquals(carRes.getId(), car.getId());
 //    }
-//}
+}
