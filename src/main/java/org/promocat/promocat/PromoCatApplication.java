1dd535855beeaf52d7d0e7750dced0ecfe39ccd6
DanLys <lyskin-2013@mail.ru>
2020-07-23 10:14:56 +0300

    CSVGenerator

--- src/main/java/org/promocat/promocat/PromoCatApplication.java
+++ src/main/java/org/promocat/promocat/PromoCatApplication.java
@@ -3,8 +3,6 @@ package org.promocat.promocat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 // TODO убрать за собой
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
