f04eceaaf4a5f4f8d6178617818b35c6dad41d25
Roman <r.devyatilov@yandex.ru>
2020-07-24 12:03:18 +0300

    soap client can send messages now

--- src/main/java/org/promocat/promocat/utils/soap/SoapGetMessageRequest.java
+++ src/main/java/org/promocat/promocat/utils/soap/SoapGetMessageRequest.java
@@ -3,15 +3,13 @@ package org.promocat.promocat.utils.soap;
 import org.promocat.promocat.utils.soap.operations.PostBindPartnerWithPhoneRequest;
 
 public class SoapGetMessageRequest extends SoapRequest {
-    public SoapGetMessageRequest(Object pojo) {
-        super(pojo);
+    public SoapGetMessageRequest(Object pojo, String token) {
+        super(pojo, token);
         this.method = "GetMessageRequest";
     }
 
     public static void main(String[] args) {
-        SoapRequest request = new SoapGetMessageRequest(
-                new PostBindPartnerWithPhoneRequest("79062587099", "INCOME_REGISTRATION")
-        );
-        request.createMessage();
+
+//        request.send();
     }
 }
--- src/main/java/org/promocat/promocat/utils/soap/SoapSendMessageRequest.java
+++ src/main/java/org/promocat/promocat/utils/soap/SoapSendMessageRequest.java
@@ -1,8 +1,8 @@
 package org.promocat.promocat.utils.soap;
 
 public class SoapSendMessageRequest extends SoapRequest {
-    public SoapSendMessageRequest(Object pojo) {
-        super(pojo);
+    public SoapSendMessageRequest(Object pojo, String token) {
+        super(pojo, token);
         this.method = "SendMessageRequest";
     }
 }
