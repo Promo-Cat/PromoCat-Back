8e1d66aa4d5a22ef738e352750a2810af983102a
Maxim <maximgran@gmail.com>
2020-09-21 17:16:43 +0300

    tax user status added

---
+++ src/main/java/org/promocat/promocat/attributes/TaxUserStatus.java
@@ -0,0 +1,8 @@
+package org.promocat.promocat.attributes;
+
+/**
+ * @author Grankin Maxim (maximgran@gmail.com) at 16:59 21.09.2020
+ */
+public enum TaxUserStatus {
+    IN_PROGRESS, FAILED, COMPLETED, TAXPAYER_UNBOUND
+}
---
+++ src/main/java/org/promocat/promocat/exception/util/tax/ApiTaxUserStatusException.java
@@ -0,0 +1,10 @@
+package org.promocat.promocat.exception.util.tax;
+
+/**
+ * @author Grankin Maxim (maximgran@gmail.com) at 17:14 21.09.2020
+ */
+public class ApiTaxUserStatusException extends RuntimeException {
+    public ApiTaxUserStatusException(final String message) {
+        super(message);
+    }
+}
