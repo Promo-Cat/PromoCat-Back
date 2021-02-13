2b04b3a8a7d4f4a77c3fc22d20ad8f0e79494ee6
DanLys <lyskin-2013@mail.ru>
2021-02-13 14:28:41 +0300

    bug fix

--- src/main/java/org/promocat/promocat/data_entities/notification_npd/NotifNPDRepository.java
+++ src/main/java/org/promocat/promocat/data_entities/notification_npd/NotifNPDRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
  * Created by Danil Lyskin at 10:59 02.02.2021
  */
 public interface NotifNPDRepository extends JpaRepository<NotifNPD, Long> {
-    Boolean existsByNotifIdAndIsOpen(String id, Boolean flag);
+    Boolean existsByNotifId(String id);
     List<NotifNPD> findAllByUserId(Long id);
 }
