118671333fd17a62f8aa15044824f5c0658ebdd6
Roman <r.devyatilov@yandex.ru>
2021-02-21 20:22:30 +0300

    hot fix2

--- src/main/java/org/promocat/promocat/data_entities/stock_activation_code/StockActivationCodeController.java
+++ src/main/java/org/promocat/promocat/data_entities/stock_activation_code/StockActivationCodeController.java
@@ -2,10 +2,13 @@ package org.promocat.promocat.data_entities.stock_activation_code;
 
 import org.promocat.promocat.data_entities.stock_activation.StockActivationService;
 import org.promocat.promocat.data_entities.user.UserService;
+import org.promocat.promocat.dto.StockActivationCodeDTO;
 import org.promocat.promocat.mapper.UserMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class StockActivationCodeController {
@@ -25,12 +28,14 @@ public class StockActivationCodeController {
 
     @RequestMapping(method = RequestMethod.POST, path = "/admin/stock/code")
     public ResponseEntity<String> activateCode(@RequestHeader("token") String token,
-                                               @RequestBody StockActivationCode code) {
+                                               @RequestBody StockActivationCodeDTO code) {
         if (code.getCode() == null) {
             return new ResponseEntity<>("Code is null", HttpStatus.NOT_FOUND);
         }
-        if (stockActivationCodeService.checkCode(code.getCode())) {
-            stockActivationService.create(userMapper.toDto(code.getUser()), code.getStockCity().getId());
+        Optional<StockActivationCodeDTO> codeOptional = stockActivationCodeService.checkCode(code.getCode());
+        if (codeOptional.isPresent()) {
+            code = codeOptional.get();
+            stockActivationService.create(userService.findById(code.getUserId()), code.getStockCityId());
             return new ResponseEntity<>("Success!", HttpStatus.OK);
         } else {
             return new ResponseEntity<>("Code is invalid", HttpStatus.NOT_FOUND);
