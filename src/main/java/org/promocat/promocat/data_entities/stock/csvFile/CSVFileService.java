77f79a3041aa775b952c9892d7a79fbcd2c37182
DanLys <lyskin-2013@mail.ru>
2021-02-23 21:46:52 +0300

    tes

--- src/main/java/org/promocat/promocat/data_entities/stock/csvFile/CSVFileService.java
+++ src/main/java/org/promocat/promocat/data_entities/stock/csvFile/CSVFileService.java
@@ -113,6 +113,7 @@ public class CSVFileService {
             Blob blob = csvFileDTO.getFile();
             try {
                 byte[] bytes = blob.getBytes(1, (int) blob.length());
+                log.info("{}", bytes.length);
                 return ResponseEntity.ok()
                         .contentType(MediaType.parseMediaType("text/csv"))
                         .header(HttpHeaders.CONTENT_DISPOSITION,
