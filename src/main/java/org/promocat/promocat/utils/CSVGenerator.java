c77e610bbf7850202771157785d011510b66e3a2
DanLys <lyskin-2013@mail.ru>
2021-02-23 21:27:21 +0300

    fix

--- src/main/java/org/promocat/promocat/utils/CSVGenerator.java
+++ src/main/java/org/promocat/promocat/utils/CSVGenerator.java
@@ -30,7 +30,7 @@ public class CSVGenerator {
         StringBuilder result = new StringBuilder();
         users.forEach(e -> result.append(e.getAccount()).append(";;;;").append(String.format("%.2f", e.getBalance())).append('\n'));
 
-        try (CSVWriter writer = new CSVWriter(new FileWriter(path.toString()), ';', '\"')) {
+        try (CSVWriter writer = new CSVWriter(new FileWriter(path.toString()), ';', '\\')) {
 
             String[] records = result.toString().split("\n");
             for (String record : records) {
