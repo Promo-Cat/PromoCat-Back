80e0a04703a1e646f8f709d30f2cb6f278ed29f4
DanLys <lyskin-2013@mail.ru>
2021-04-04 15:33:20 +0300

    del

--- src/main/java/org/promocat/promocat/data_entities/stock/StockService.java
+++ src/main/java/org/promocat/promocat/data_entities/stock/StockService.java
@@ -211,7 +211,7 @@ public class StockService {
                     y.setStockCityId(null);
                     users.add(y);
                     userRepository.save(userMapper.toEntity(y));
-                    applicationContext.getBean(UserService.class).subscribeUserOnDefaultTopics(y);
+//                    applicationContext.getBean(UserService.class).subscribeUserOnDefaultTopics(y);
                 });
         csvGenerator.generate(path, users);
 
