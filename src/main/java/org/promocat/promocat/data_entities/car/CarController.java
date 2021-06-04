228d882279ac74f2b964c9b41ceaee5245e188a2
DanLys <lyskin-2013@mail.ru>
2021-06-04 20:09:16 +0300

    fix

--- src/main/java/org/promocat/promocat/data_entities/car/CarController.java
+++ src/main/java/org/promocat/promocat/data_entities/car/CarController.java
@@ -167,7 +167,7 @@ public class CarController {
     public ResponseEntity<CarDTO> uploadSts(@RequestHeader("token") String token,
                                             @PathVariable("id") Long carId,
                                             @RequestParam("poster") MultipartFile file) {
-        log.info("File size: {} ", file.getSize());
+        log.error("File size: {} ", file.getSize());
         CarDTO carDTO = carService.findById(carId);
         UserDTO userDTO = userService.findByToken(token);
         if (userDTO.getCarId().equals(carDTO.getId())) {
