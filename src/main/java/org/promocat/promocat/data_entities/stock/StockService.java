c24cc821959c923c6505dc9f667285f0e345c1f5
DanLys <lyskin-2013@mail.ru>
2021-07-07 18:48:40 +0300

    fix

--- src/main/java/org/promocat/promocat/data_entities/stock/StockService.java
+++ src/main/java/org/promocat/promocat/data_entities/stock/StockService.java
@@ -217,6 +217,7 @@ public class StockService {
                         unbannedUsers.add(y);
                     } else {
                         y.setStatus(UserStatus.FULL);
+                        applicationContext.getBean(UserBanService.class).deleteFromBan(y.getId(), stockDTO.getId());
                     }
 
                     allUsersFromStock.add(y);
